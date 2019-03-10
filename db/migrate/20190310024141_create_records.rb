758fc52ee6e1b1e325af0d516add07c339f21dc3
kaileighrose <kaileighrose@gmail.com>
2019-03-09 22:17:19 -0500

    add model relationships and update migration

--- db/migrate/20190310024141_create_records.rb
+++ db/migrate/20190310024141_create_records.rb
@@ -1,9 +1,10 @@
 class CreateRecords < ActiveRecord::Migration[5.2]
   def change
     create_table :records do |t|
+      t.belongs_to :user, index: true
       t.string :artist
       t.string :title
-      t.number :year
+      t.integer :year
       t.text :condition
 
       t.timestamps
